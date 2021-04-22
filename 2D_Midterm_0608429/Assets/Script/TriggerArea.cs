84c7d28a3a8fdd5ecb74bb545cc9a1ca6144f200
fluffyovo <71476490+fluffyovo@users.noreply.github.com>
2021-04-23 06:42:28 +0800

    新增TriggerAreaC#

---
+++ 2D_Midterm_0608429/Assets/Script/TriggerArea.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TriggerArea : MonoBehaviour
+{
+    [Header("想吃甜甜圈的小熊")]
+    public GameObject bearmi;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "甜甜圈")
+        {
+            bearmi.SetActive(false);
+            
+        }
+        Destroy(gameObject);
+    }
+}
