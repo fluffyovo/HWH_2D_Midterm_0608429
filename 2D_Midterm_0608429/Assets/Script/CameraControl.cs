2b3a1ed7628f3ccc9c20294ce2d81ff977c97287
fluffyovo <71476490+fluffyovo@users.noreply.github.com>
2021-04-23 05:56:02 +0800

    場景布置與道具互動

---
+++ 2D_Midterm_0608429/Assets/Script/CameraControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [Header("攝影機速度"), Range(0, 50)]
+    public float speed = 1.5f;
+    [Header("上下邊界")]
+    public Vector2 limitY = new Vector2(-5, 5);
+    [Header("左右邊界")]
+    public Vector2 limitX = new Vector2(-5, 5);
+    [Header("玩家座標")]
+    public Transform player;
+
+    private void Update()
+    {
+        Track();
+    }
+
+    /// <summary>
+    /// 攝影機追蹤
+    /// </summary>
+    private void Track()
+    {
+        Vector3 posCam = transform.position;   // 取得 攝影機座標
+        Vector3 posPla = player.position;      // 取得 玩家座標
+
+        // Lerp 差值 使A值逐漸靠近B值
+        posCam = Vector3.Lerp(posCam, posPla, 0.5f * speed * Time.deltaTime);
+        posCam.z = -10;    //使Z維持在同一個值(高度)，避免鏡頭貼住地面看不見
+
+        // Mathf.Clamp (要夾住的值，最小值，最大值）
+        posCam.x = Mathf.Clamp(posCam.x, limitX.x, limitX.y);
+        posCam.y = Mathf.Clamp(posCam.y, limitY.x, limitY.y);
+
+        transform.position = posCam;           // 更新攝影機座標
+    }
+
+}
---
+++ 2D_Midterm_0608429/Assets/Script/Player.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    [Header("角色名稱"), Tooltip("這是角色名稱")]
+    public string cName = "波米";
+    [Header("移動速度"), Range(1, 100)]
+    public float speed = 10.5f;
+    [Header("攻擊"), Range(1, 100)]
+    public float attack = 20f;
+
+    [Header("蛋糕"), Range(0, 1000)]
+    public int cake = 0;
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+    [Header("變形元件")]
+    public Transform tra;
+    [Header("動畫元件")]
+    public Animator ani;
+    [Header("偵測攻擊範圍")]
+    public float rangeAttack = 1.2f;
+    [Header("音效來源")]
+    public AudioSource aud;
+    [Header("攻擊音效")]
+    public AudioClip soundAttack;
+
+
+    [Header("吃蛋糕音效")]
+    public AudioClip SoundEat;
+    [Header("吃蛋糕數量")]
+    public Text textCake;
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.2f);
+
+        Gizmos.DrawSphere(transform.position, rangeAttack);
+    }
+
+    private void Move()
+    {
+        float h = joystick.Horizontal;
+        float v = joystick.Vertical;
+        /*if(h == 0.5f)
+        {
+            ani.SetFloat("靜止", h);
+        }*/
+        tra.Translate(h * speed * Time.deltaTime, v * speed * Time.deltaTime, 0);
+        ani.SetFloat("水平", h);
+        //ani.SetFloat("垂直", v);
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    public void Attack()
+    {
+        aud.PlayOneShot(soundAttack, 0.6f);
+
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, rangeAttack, -transform.up, 0, 1 << 8);
+
+        if (hit && hit.collider.tag == "道具")
+        {
+            hit.collider.GetComponent<item>().DropProp();
+            print("碰到的物件:" + hit.collider.name);
+
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        if (collision.tag == "蛋糕")
+        {
+            cake++;
+            aud.PlayOneShot(SoundEat);
+            Destroy(collision.gameObject);
+            textCake.text = "：" + cake;
+
+        }
+    }
+
+
+}
